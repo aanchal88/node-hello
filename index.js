558bcae42122d095d0a3f09262001f37b04722c3
aanchal88 <76724203+aanchal88@users.noreply.github.com>
2021-02-18 14:08:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node from aanchal!\n'
+  const msg = 'Hello Node from \n'
   res.end(msg);
 });
 
